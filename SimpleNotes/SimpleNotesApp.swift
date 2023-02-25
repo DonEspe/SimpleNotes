79be3761b6a742f495b9cbe199d28afb74762629
Don Espe <Don.Espe@gmail.com>
2023-02-25 16:20:06 -0600

    Initial Commit

---
+++ SimpleNotes/SimpleNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleNotesApp.swift
+//  SimpleNotes
+//
+//  Created by Don Espe on 2/25/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
